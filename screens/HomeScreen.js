2e3f51857540deeef47b0cf58dfd7b8b9332a9db
okarslan <okarslan@dabnet.com.tr>
2024-05-14 01:08:16 +0300

    home page designed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Khan page!</Text>
+      <Text>You entered succesfully.</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    color:'blue',
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+});
